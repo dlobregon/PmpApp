5d8bbf4750749f25da170a462b9a877f9671eb8d
dlobregon <troyanrex@gmail.com>
2019-03-25 00:44:19 -0600

    funcionalidad de bar chart

--- components/Dashboard/ChartScreen.js
+++ components/Dashboard/ChartScreen.js
@@ -2,29 +2,95 @@ import React, { Component } from "react";
 import { 
     View,
     Text,
-    StyleSheet
+    StyleSheet, 
+    ActivityIndicator,
+    ScrollView, 
+    SafeAreaView
 } from "react-native";
 import {
     VictoryBar,
+    VictoryChart
   } from "victory-native";
+  import { VictoryTheme } from "victory-core";
 
+
+const makeActual =(actual_planificado, actual_real)=>{
+    let valorActual=[
+        {tipo:"Real", valor:actual_real, label:actual_real, fill:"blue"},
+        {tipo:"Planificado", valor:actual_planificado, label:actual_planificado,  fill:"orange"}
+    ];
+    return valorActual;
+}
+  
 class ChartScreen extends Component {
+    constructor(props){
+        super(props);
+        this.state ={ isLoading: true}
+    }
+    componentDidMount(){
+        return fetch('http://192.168.1.14:3000/dashboard-indicadores/')
+          .then((response) => response.json())
+          .then((responseJson) => {
+    
+            this.setState({
+              isLoading: false,
+              //dataSource: responseJson.movies,
+              total_planificado:responseJson.total_planificado,
+              actual_planificado:responseJson.actual_planificado,
+              actual_real: responseJson.actual_real,
+              valorActual: makeActual(responseJson.actual_planificado, responseJson.actual_real),
+              valorTotal:makeActual(responseJson.total_planificado, responseJson.actual_real)
+            }, function(){
+    
+            });
+    
+          })
+          .catch((error) =>{
+            console.error(error);
+          });
+      }
+
+
     render() {
+        if(this.state.isLoading){
+            return(
+              <View style={{flex: 1, padding: 20}}>
+                <ActivityIndicator/>
+              </View>
+            )
+        }
         return (
-            <View style={styles.container}>
-                <Text>Some random Chart</Text>
-                <VictoryBar
-            style={{ data: { fill: "tomato", opacity: 0.5 } }}
-            data={[
-              { x: 15, y: 20, label: 1, fill: "red" },
-              { x: 25, y: 30, label: 2, fill: "orange" },
-              { x: 35, y: 65, label: 3, fill: "gold" },
-              { x: 40, y: 50, label: 4, fill: "blue" },
-              { x: 45, y: 40, label: 5, fill: "cyan" },
-              { x: 50, y: 30, label: 6, fill: "green" }
-            ]}
-          />
-            </View>
+            <SafeAreaView style={{flex:1}}>
+                <ScrollView scrollEventThrottle={16}>
+                    <View style={{borderBottomColor:"gray", borderBottomWidth:0.5}}>
+                        <Text>Ejecutado a la fecha</Text>
+                        <VictoryChart  theme={VictoryTheme.material} domainPadding={80} >
+                            <VictoryBar
+                                style={{ data: {  fill: (d) => d.tipo === "Real" ? "#66bb6a" : "#03a9f4", opacity: 1 } }}
+                                data={this.state.valorActual}
+                                alignment="middle"
+                                x="tipo" y="valor"
+                                barWidth={60}
+                                animate={{ duration: 1000,onLoad: { duration: 500 }}}
+                            />
+                        </VictoryChart>
+                    </View>     
+                    <View>
+                        <Text>Ejecutado total</Text>
+                        <VictoryChart  theme={VictoryTheme.material} domainPadding={80} >
+                            <VictoryBar
+                                style={{ data: {  fill: (d) => d.tipo === "Real" ? "#66bb6a" : "#03a9f4", opacity: 1 } }}
+                                data={this.state.valorTotal}
+                                alignment="middle"
+                                x="tipo" y="valor"
+                                barWidth={60}
+                                animate={{ duration: 1000,onLoad: { duration: 500 }}}
+                            />
+                        </VictoryChart>
+                    </View>       
+                </ScrollView>
+            </SafeAreaView>
+                
         );
     }
 }
