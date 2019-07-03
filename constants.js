269dff682976dc5d0dc9394fb016aa376e6822c6
Diego López <troyanrex@gmail.com>
2019-07-03 17:21:24 -0600

    Add devicestorage import

--- constants.js
+++ constants.js
@@ -1,4 +1,7 @@
-const ApiUrl = "http://192.168.43.14:3000"
+
+
+import deviceStorage from './components/Common/MyStorage'
+const ApiUrl = "http://192.168.42.11:3000"
 //para la flatlist de las tareas
 const PAGE_SIZE= 20;
 
